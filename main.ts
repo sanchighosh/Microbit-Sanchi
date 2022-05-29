edefc78fc5630e34d8be0689fd97ac1c0613175f
sanchighosh <104710286+sanchighosh@users.noreply.github.com>
2022-05-29 17:52:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,63 @@
-basic.forever(function () {
-    basic.showLeds(`
-        # # . # #
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        # # # # #
-        `)
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+    control.waitMicros(1306)
+    basic.showString("youWIN!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("sorry but you lose")
+})
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            . # # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (number == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+    	
+    }
 })
